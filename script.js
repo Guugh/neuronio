bcf0901335d47e39f2b59aa80787d2d24c8572bd
Gustavo Bettoi <gustavo.betcav@gmail.com>
2023-11-28 22:27:38 -0300

    teste 1

---
+++ script.js
@@ -0,0 +1,22 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+window.addEventListener('scroll', function()
+{
+let scroll = document.querySelector('.scrolltop')
+scroll.classList.toggle('active', window.scrollY > 10)
+})
